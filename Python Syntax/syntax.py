4bb4f3d1d02f4202f4450e99375d3b1d5e5fc679
ItsmeLee <arsmagica90@gmail.com>
2021-01-25 12:27:52 +0900

    Create syntax.py file in main folder.
    Create syntax.py file in main folder.
    In syntax.py file usually write basic syntax in python languages.

---
+++ Python Syntax/syntax.py
@@ -0,0 +1,25 @@
+
+## variables (number)
+a = 2
+b = 3
+print(a+b)
+
+## Variables ( text )
+
+a_string = "like this"
+print(string)
+
+# Variables( float )
+
+a_float = 3.12
+print(a_float)
+
+# Boolean
+
+a = True
+b = False
+print(a, b)
+
+# None
+a_none = None
+print(a_none)
