f36553493398dcc65b79952344c87ae35a3a61c1
ItsmeLee <arsmagica90@gmail.com>
2021-01-26 21:35:01 +0900

    Practice if else and and or syntaxs
    Practice if else, and or syntaxs in syntax.py file.

--- Python Syntax/syntax.py
+++ Python Syntax/syntax.py
@@ -123,3 +123,21 @@ print(divide("10", 2))
 print(remind("10", 2))
 print(power("10", 2))
 """
+
+# if else and or
+# age check
+
+
+def age_check(age):
+    print(f"you are {age}")
+    if age < 18:
+        print("you can drink")
+    elif age == 18 or age == 19:
+        print("you are new to this!")
+    elif age > 20 and age < 25:
+        print("you are still kind of young")
+    else:
+        print("enjoy your drink")
+
+
+age_check(18)
