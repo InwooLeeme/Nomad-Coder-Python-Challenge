0c213d14211ea29de63a677547aaffaae3227a0c
ItsmeLee <arsmagica90@gmail.com>
2021-01-29 18:28:43 +0900

    Get some elements from url
    Get some td tags using BeautifulSoup
    The bringed td tags are stored sequentially in the countrylist and codelist, respectively.

--- Day5/day5.py
+++ Day5/day5.py
@@ -1,6 +1,15 @@
-
 import requests
 from bs4 import BeautifulSoup
 
-
 url = "https://www.iban.com/currency-codes"
+
+request = requests.get(url)
+
+country_list = BeautifulSoup(
+    request.text, "html.parser").select("td:nth-child(1)")
+
+code_list = BeautifulSoup(
+    request.text, "html.parser").select("td:nth-child(3)")
+
+
+ask_request()
