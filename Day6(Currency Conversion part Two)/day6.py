50e99a29ca2ea061fdd8b53c07826c4c363699d3
ItsmeLee <arsmagica90@gmail.com>
2021-01-30 18:17:59 +0900

    Get name and code from table
    Get name,code and currency from the table.

--- Day6(Currency Conversion part Two)/day6.py
+++ Day6(Currency Conversion part Two)/day6.py
@@ -15,7 +15,21 @@ table = html.find("table")
 
 rows = table.find_all("tr")[1:]
 
-print(rows)
+countries = []
+
+for row in rows:
+    td = row.find_all("td")
+    name = td[0].text
+    currency = td[1].text
+    code = td[2].text
+    if(name and code):
+        if(currency != "No universal currency"):
+            country = {
+                "name": name,
+                "code": code
+            }
+            countries.append(country)
+
 
 """
 Use the 'format_currency' function to format the output of the conversion
