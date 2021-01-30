6d868953dc056fe99a598bf0f8512f8f78d8942f
ItsmeLee <arsmagica90@gmail.com>
2021-01-30 15:56:19 +0900

    Find td tags in html
    Find the td tags in extracted html.

--- Day5/day5.py
+++ Day5/day5.py
@@ -5,15 +5,8 @@ url = "https://www.iban.com/currency-codes"
 
 request = requests.get(url)
 
-country_list = BeautifulSoup(
-    request.text, "html.parser").select("td:nth-child(1)")
+html = BeautifulSoup(request.text, "html.parser")
 
-code_list = BeautifulSoup(
-    request.text, "html.parser").select("td:nth-child(3)")
+table = html.find("table")
 
-country_name = []
-code_name = []
-
-for name in country_list:
-    country_name.append(name.text)
-country_list = country_name
+print(table)
