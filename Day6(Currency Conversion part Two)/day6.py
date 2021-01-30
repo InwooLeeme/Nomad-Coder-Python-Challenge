5555e2e06a594da8e9847e29aa094222e677ce6d
ItsmeLee <arsmagica90@gmail.com>
2021-01-30 18:12:28 +0900

    Extract html and table
    Extract html and table from the url.

--- Day6(Currency Conversion part Two)/day6.py
+++ Day6(Currency Conversion part Two)/day6.py
@@ -5,9 +5,21 @@ from babel.numbers import format_currency
 
 # os.system("clear")
 
+url = "https://www.iban.com/currency-codes"
+
+request = requests.get(url)
+
+html = BeautifulSoup(request.text, "html.parser")
+
+table = html.find("table")
+
+rows = table.find_all("tr")[1:]
+
+print(rows)
+
 """
 Use the 'format_currency' function to format the output of the conversion
 format_currency(AMOUNT, CURRENCY_CODE, locale="ko_KR" (no need to change this one))
 """
 
-print(format_currency(5000, "KRW", locale="ko_KR"))
+#print(format_currency(5000, "KRW", locale="ko_KR"))
