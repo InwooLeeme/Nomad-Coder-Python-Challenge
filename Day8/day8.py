3c1e627b3fc1900407512f9630a8a35eacad3d49
ItsmeLee <arsmagica90@gmail.com>
2021-02-01 11:48:08 +0900

    Extract html from alba_url
    Extract html from abla_url using BeautifulSoup.

--- Day8/day8.py
+++ Day8/day8.py
@@ -5,3 +5,11 @@ from bs4 import BeautifulSoup
 
 # os.system("clear")
 alba_url = "http://www.alba.co.kr"
+
+alba_request = requests.get(alba_url)
+
+abla_html = BeautifulSoup(alba_request.text, "html.parser")
+
+alba_ul = abla_html.find_all("ul", {"class": "goodsBox"})[1:]
+
+print(alba_ul)
