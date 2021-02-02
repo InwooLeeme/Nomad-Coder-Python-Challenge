dd32b1edcf19b0b4a7ac3bacd8af6607ef095052
ItsmeLee <arsmagica90@gmail.com>
2021-02-02 14:34:35 +0900

    Render html template
    Practiced how to use html file in flask.

--- Day9,10/flask2.py
+++ Day9,10/flask2.py
@@ -1,10 +1,10 @@
-from flask import Flask
+from flask import Flask, render_template
 app = Flask(__name__)
 
 
 @app.route("/")
 def hello():
-    return "Hello World!!"
+    return render_template("home.htm")
 
 
 app.run(host='127.0.0.1', port=5000)
