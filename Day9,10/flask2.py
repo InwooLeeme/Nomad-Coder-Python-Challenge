10e1ce28cd968a02d85d186267961ab136715091
ItsmeLee <arsmagica90@gmail.com>
2021-02-04 12:03:11 +0900

    Add four route and functions
    Add four route and each functions.
    When user enter the main page, in main page show the popular news in default.

--- Day9,10/flask2.py
+++ Day9,10/flask2.py
@@ -1,10 +1,74 @@
-from flask import Flask, render_template
-app = Flask(__name__)
+import requests
+from flask import Flask, render_template, request
+import json
+
+base_url = "http://hn.algolia.com/api/v1"
+
+# This URL gets the newest stories.
+new = f"{base_url}/search_by_date?tags=story"
+
+# This URL gets the most popular stories
+popular = f"{base_url}/search?tags=story"
+
+
+# This function makes the URL to get the detail of a storie by id.
+# Heres the documentation: https://hn.algolia.com/api
+def make_detail_url(id):
+    return f"{base_url}/items/{id}"
+
+
+db = {}
+app = Flask("DayNine")
+
+popular_articles = requests.get(popular).json()['hits']
+new_articles = requests.get(new).json()['hits']
+
+
+def find(request):
+    query = request.args.get('order_by')
+    if(query == 'new'):
+        data = new_articles
+        db = new_articles
+        title = "New"
+    elif(query == 'popular'):
+        data = popular_articles
+        db = popular_articles
+        title = "Popular"
+    else:
+        data = popular_articles
+        db = popular_articles
+        title = "Popular"
+    return render_template(
+        "index.html",
+        hits=data,
+        query=query
+    )
 
 
 @app.route("/")
-def hello():
-    return render_template("home.htm")
+def home():
+    return find(request)
 
 
-app.run(host='127.0.0.1', port=5000)
+@app.route("/?order_by=new")
+def new():
+    return find(request)
+
+
+@app.route("/?order_by=popular")
+def popular():
+    return find(request)
+
+
+@app.route("/<id>"):
+def detail(id):
+    detail_url = make_detail_url(id)
+    request_detail = requests.get(detail_url).json()
+    db = request_detail
+    return render_template(
+        "detail.html",
+        data=request_detail
+    )
+
+
+app.run(host="localhost", port="5000")
